ac3405fa6b97444cc7c31fa69aa98a1163166cfb
Aaron Dantley <aadantley@gmail.com>
2025-01-28 23:48:10 -0500

    style (navbar): making navbar fully white instead of partially gray

--- components/common/NavBar.js
+++ components/common/NavBar.js
@@ -44,7 +44,7 @@ export default function NavBar() {
   };
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 shadow-sm bg-white/95 backdrop-blur-sm">
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-white shadow-sm backdrop-blur-sm">
       <div className="flex flex-row items-center justify-between p-4 mx-10 text-black">
         {/* Left section: Logo and Text */}
         <div className="flex-1">
